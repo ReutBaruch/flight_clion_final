01f12d27eb02f9006f9e0991cfccde67a92cbb4b
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 21:59:06 +0200

    Update CommandExpression.h

--- CommandExpression.h
+++ CommandExpression.h
@@ -6,7 +6,11 @@
 #include "CreateExpression.h"
 #include "SymbolTable.h"
 
+/**
+ * CommandExpression class inheritors Expression
+ */
 class CommandExpression: public Expression {
+    //members
     Command* command;
     vector<string>::iterator vectorIt;
     SymbolTable* symbols;
@@ -14,6 +18,11 @@ class CommandExpression: public Expression {
 
 
 public:
+    /**
+     * constructor.
+     * @param it  iterator
+     * @param command1 Command
+     */    
     CommandExpression(vector<string>::iterator &it, Command* command1, SymbolTable* symbol,
             CreateExpression* createExp){
         this->command = command1;
@@ -21,10 +30,22 @@ public:
         this->symbols = symbol;
         this->createExpression = createExp;
     }
-
+    
+/**
+ * calculate the value of the expression
+ * @return the value after calculate
+ */
     double calculate();
+      /**
+ *
+ * @param assignment map
+ * @return the result of the expression in numbers
+ */
     double calculate(map<string, double> &assignment);
 
+    /**
+* distructor
+*/
     ~CommandExpression(){ };
 };
 
