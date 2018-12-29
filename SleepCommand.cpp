799eb4e2225cf80f5df4db6448252f8d13b7c2e6
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 22:27:35 +0200

    Update SleepCommand.cpp

--- SleepCommand.cpp
+++ SleepCommand.cpp
@@ -3,7 +3,11 @@
 #include "SleepCommand.h"
 
 
-
+    /**
+     * sleep
+     * @param vectorIt vector to run on
+     * @return nothing
+     */
 int SleepCommand::execute(vector<string>::iterator &vectorIt){
     int num = atoi((*vectorIt).c_str());
     sleep(num);
