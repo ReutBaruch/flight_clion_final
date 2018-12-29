f80104849120529e7f3d3f49e77eb9c58da127aa
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 22:03:42 +0200

    Update IfCommand.h

--- IfCommand.h
+++ IfCommand.h
@@ -5,19 +5,35 @@
 #include "CommandExpression.h"
 #include "BoolExpression.h"
 #include "SymbolTable.h"
-
+/**
+ * ifCommand class inheritors ConditionParser
+ */
 class IfCommand: public Command {
     list<CommandExpression*> commandExpressions;
     BoolExpression* boolExpression;
     SymbolTable* symbolTable;
 
 public:
+    /**
+     * constructor
+     * @param boolExpres boolExpression
+     * @param listExp commandExpressions
+     * @param symbolTable symbolTable
+     */
     IfCommand(BoolExpression *boolExpres, list<CommandExpression*> listExp, SymbolTable* symbolTable) {
         this->commandExpressions = listExp;
         this->boolExpression = boolExpres;
         this->symbolTable=symbolTable;
     }
+    /**
+     * if the condition true- do the next.
+     * @param script the iterator
+     * @return 0 when finish
+     */
     virtual int execute(vector<string>::iterator &vectorIt);
+    /**
+    * distructor
+    */
     ~IfCommand(){};
 
 };
