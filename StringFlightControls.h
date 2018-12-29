df1571f67634ddeae69b6d9ff0f86f8e9b58b1a3
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 22:19:06 +0200

    Update StringFlightControls.h

--- StringFlightControls.h
+++ StringFlightControls.h
@@ -5,26 +5,46 @@
 #include <list>
 
 using namespace std;
-
+/**
+ * string to control the flight class
+ */
 class StringFlightControls {
+    //memmbers
     list<string>controlCommand;
 public:
+    /**
+     * constructor
+     */
     StringFlightControls(){}
 
+    /**
+     * add a control
+     * @param s contriol string
+     */
     void setControl(string s){
         this->controlCommand.push_back(s);
     }
 
+    /**
+     * get a control from the list
+     * @return control as a string
+     */
     string getControl(){
         string control = this->controlCommand.front();
         this->controlCommand.pop_front();
         return control;
     }
 
+    /**
+     * if the list is empty
+     * @return true if empty
+     */
     bool isEmpty(){
         return this->controlCommand.empty();
     }
-
+    /**
+     * distructor
+     */
     ~StringFlightControls(){};
 };
 
