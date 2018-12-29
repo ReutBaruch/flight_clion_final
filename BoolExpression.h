8fe571e246838d6cbf85fbd81dab1127514e220b
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 21:03:37 +0200

    Update BoolExpression.h

--- BoolExpression.h
+++ BoolExpression.h
@@ -5,7 +5,11 @@
 #ifndef PROJECT1_BOOLEXPRESSION_H
 #define PROJECT1_BOOLEXPRESSION_H
 
+/**
+* BoolExpression class
+*/
 class BoolExpression  {
+    //members
     Expression* leftArgument;
     Expression* rightArgument;
     string action;
@@ -13,15 +17,32 @@ class BoolExpression  {
     CreateExpression* createExpression;
 public:
 
+    /**
+ * constructor
+ * @param stringCondition the condition by string
+ * @param symbol the symbolTable
+ */
     BoolExpression(string stringCondition, SymbolTable* symbol) {
         this->symbols = symbol;
         this->createBoolExpression(stringCondition);
         //this->createExpression = createExp;
     }
 
+         /**
+ * the function gets a condition in string and makes it to expression.
+ * @param boolCondition the condition
+ */
     void createBoolExpression(string conditionToCheck);
+      /**
+ * the function calculate the condition and returns the answer
+ * @param symbolMap the symbol map
+ * @return true / false according to the condition
+ */
     bool calculateBool(map <string,double> symbolMap);
 
+   /**
+   * distcructor
+   */
     ~BoolExpression(){
         delete this->createExpression;
     }
