dc6e6e3289e9009df38df546cd5876ed9f932eaa
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 22:10:58 +0200

    Update ConditionParser.h

--- ConditionParser.h
+++ ConditionParser.h
@@ -9,7 +9,11 @@
 #define PROJECTNUM1_CONDITIONPARSER_H
 
 
+/**
+ * ConditionParser inheritors Command
+ */
 class ConditionParser : public Command {
+    //members
     list<CommandExpression*> expressionCommandList;
     CommandMap* commandMap;
     BoolExpression* boolExpression;
@@ -20,11 +24,26 @@ class ConditionParser : public Command {
 
 public:
 
+   /**
+    * constructor
+    */
     ConditionParser(CommandMap* commandMap,SymbolTable* symbolMap, CreateExpression* createExp);
 
+     /**
+     * parser the condition
+     * @param script iterator
+     * @return 0 when finish
+     */
     virtual int execute(vector<string>::iterator &vectorIt);
+     /**
+     * create new command
+     * @param vectorIt the iterator
+     */
     void createCommand(vector<string>::iterator &vectorIt);
 
+        /**
+    *distructor
+    */
     virtual ~ConditionParser(){
         list<Command*>::iterator itr;
         for(itr = toDelete.begin(); itr != toDelete.end(); itr++){
