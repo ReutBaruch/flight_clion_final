a001d2eeb84af64aaea349feb09e99e1e7cf0584
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 22:05:46 +0200

    Update FlightValueMap.h

--- FlightValueMap.h
+++ FlightValueMap.h
@@ -7,19 +7,34 @@
 #include <string.h>
 
 using namespace std;
-
+/**
+* FlightValueMap class
+*/
 class FlightValueMap {
     map<string,double> mapOfFlightValue;
     map<int,string> indexOfBind;
     string save;
 public:
+    /**
+    * constructor
+    */
     FlightValueMap() {
         this->initalize();
     }
-
+    /**
+     * initalize the map
+     */
     void initalize();
+    /**
+     * update the map according to the string
+     * @param values  the value to update to
+     */
     void updateMap(string values);
+    
     map<string,double> getFlightVauleMap();
+    /**
+    * distructor
+    */
     ~FlightValueMap(){}
 
 
