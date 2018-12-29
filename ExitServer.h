169aa90a0b0db54b9c49ea5471448db369fb7e7a
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 22:08:35 +0200

    Update ExitServer.h

--- ExitServer.h
+++ ExitServer.h
@@ -1,20 +1,34 @@
 #ifndef PROJECT1_EXITSERVER_H
 #define PROJECT1_EXITSERVER_H
 
-
+/**
+*ExitServer class
+*/
 class ExitServer {
     bool toExit;
 
 public:
+    /**
+    * constructor
+    */
     ExitServer(){
         this->toExit = false;
     }
+    /**
+    * @param flag- change the exit type
+    */
     void setExit(bool flag){
         this->toExit  = flag;
     }
+    /**
+    *@ return to exit or not
+    */
     bool getExit() const{
         return this->toExit;
     }
+    /**
+    * distructor
+    */
     ~ExitServer(){}
 };
 
