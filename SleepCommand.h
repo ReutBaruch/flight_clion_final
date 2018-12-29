3b63eb89dbe958baf4bbfac10003bf69ae6ecb06
ReutBaruch <46083895+ReutBaruch@users.noreply.github.com>
2018-12-29 22:20:55 +0200

    Update SleepCommand.h

--- SleepCommand.h
+++ SleepCommand.h
@@ -3,9 +3,21 @@
 
 #include "Command.h"
 
+/**
+ * sleep class
+ */
 class SleepCommand: public Command {
+
 public:
+    /**
+     * sleep
+     * @param vectorIt vector to run on
+     * @return nothing
+     */
     virtual int execute(vector<string>::iterator &vectorIt);
+    /**
+     * distructor
+     */
     virtual ~SleepCommand(){};
 
 };
